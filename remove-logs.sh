bf1f7dc0a0ce638c19f57528eb30c0ca0757aa7a
Scholz, Bjorn <scholz@uchicago.edu>
2016-05-06 11:55:05 -0500

    Added CondorFiles to log cleaning

--- remove-logs.sh
+++ remove-logs.sh
@@ -2,6 +2,8 @@
 rm -rf /home/bjs66/Errs
 rm -rf /home/bjs66/Logs
 rm -rf /home/bjs66/Outs
+rm -rf /home/bjs66/CondorFiles
 mkdir /home/bjs66/Errs
 mkdir /home/bjs66/Logs
 mkdir /home/bjs66/Outs
+mkdir /home/bjs66/CondorFiles
