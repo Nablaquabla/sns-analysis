cd39cc70ea75d163a950c38b7a24f44e5b4e52af
Scholz, Bjorn <scholz@uchicago.edu>
2016-05-09 11:38:43 -0500

    Added python script to copy multiple runs

---
+++ copy-data-from-sns.py
@@ -0,0 +1,64 @@
+import os
+import time as tm
+
+# Handles the creation of condor files for a given set of directories
+# -----------------------------------------------------------------------------
+def createCondorFile(RUNTIME):
+    with open('/home/bjs66/CondorFiles/copy-%s.condor'%(RUNTIME),'w') as f:
+        f.write('Executable = /home/bjs66/GitHub/sns-analysis/copy-data-from-sns.sh')
+        f.write('Arguments = %s'%RUNTIME)
+        f.write('Universe = vanilla')
+        f.write('Getenv = true')
+        f.write('Log = ../../Logs/copy.log')
+        f.write('Output = ../../Outs/copy.out')
+        f.write('Error = ../../Errs/copy.err')
+        f.write('Notification = never')
+        f.write('+Department  = Physics')
+        f.write('should_transfer_files = NO')
+        f.write('Queue')
+        
+# Main function handling all internals
+# -----------------------------------------------------------------------------
+def main():       
+    # Choose run to copy
+#    run = 'Run-16-02-02-16-26-26'
+    run = 'Run-16-02-15-13-46-34'
+#    run = 'Run-16-02-29-11-54-20'
+#    run = 'Run-16-03-09-13-00-14'
+#    run = 'Run-16-03-22-18-09-33' 
+              
+    print run              
+    createCondorFile(run)
+    cmd = 'condor_submit /home/bjs66/CondorFiles/copy-%s.condor'%(run)
+    print cmd
+    #os.system(cmd)
+ 
+if __name__ == '__main__':
+    main()
+
+    
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
