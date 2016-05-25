da854889335997c0790a84bc200032e1f0d2eb26
Nablaquabla <nablaquabla@live.com>
2016-05-25 14:02:33 -0500

    Removed all debug output

--- sns-analysis-v3.cpp
+++ sns-analysis-v3.cpp
@@ -172,8 +172,6 @@ int main(int argc, char* argv[])
         return 1;
     }
 
-	std::cout << argv[1] << " " << argv[2] << " " << argv[3] << " " << argv[4] << " " << argv[5] << std::endl;
-
 	unsigned int BG_PT[2] = {};
 	unsigned int BG_ROI[2] = {};
 	unsigned int S_PT[2] = {};
@@ -245,8 +243,6 @@ int main(int argc, char* argv[])
 		// Prepare paths to zipped and unzipped files
 		current_zip_file = main_dir + "/" + time_files[current_time] + ".zip";
 
-		std::cout << current_zip_file << std::endl;
-
 		time_name_in_zip = time_files[current_time];
 	}
 
@@ -287,7 +283,6 @@ int main(int argc, char* argv[])
 	infoOut.open((out_dir + "/" + fileName(atoi(time_name_in_zip.c_str()), "I-")).c_str(), std::ofstream::out | std::ofstream::trunc);
             
 	// Begin data processing if file has been properly opened
-	std::cout << "ERROR?! " << err << std::endl;
 	if(err == 0)
 	{
 		waveformCtr = 0;
@@ -314,7 +309,6 @@ int main(int argc, char* argv[])
 			{
 				break;
 			}
-			std::cout << no_samples << std::endl;
 			// ----------------------------------------------------------------
 			//  Process XYZ consecutive waveforms without encountering another
 			// LabView header inbetween
