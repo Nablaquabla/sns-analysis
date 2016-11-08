8792b28fc6aa0fbbef8e1d51c3414c6bc9e3e8e2
Nablaquabla <nablaquabla@live.com>
2016-11-08 14:31:07 -0600

    removed old files

--- compile.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-g++ sns-analysis-multi-submit.cpp -lzip -L /home/bjs66/usr/bin/libzip/lib/ -I /home/bjs66/usr/bin/libzip/include/ -I /home/bjs66/usr/bin/libzip/lib/libzip/include/ -o sns-analysis-multi-submit
