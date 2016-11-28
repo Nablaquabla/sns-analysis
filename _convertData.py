4f6a24cf12abae0955a21a9eab7434c053ae4506
Nablaquabla <nablaquabla@live.com>
2016-11-28 12:03:26 -0500

    Added condor python analysis

---
+++ _convertData.py
@@ -0,0 +1,200 @@
+#!/home/bjs66/anaconda2/bin/python
+
+import sys
+import h5py
+import numpy as np
+import os
+
+# ============================================================================
+#                       Full timestamp conversion
+# ============================================================================
+def convertTimestamp(x):
+    return datetime.datetime.strptime(str(x), '%y%m%d%H%M%S%f').date()    
+ctv = np.vectorize(convertTimestamp)
+
+# ============================================================================
+#                       Full timestamp conversion
+# ============================================================================
+def convertTime(number):
+    multiplicators = [0.01,0.1,1.0,10.0,60.0,600.0,3600.0,36000.0]
+    s = 0
+    for i in xrange(8):
+        s += multiplicators[i] * (number % 10)
+        number //= 10
+    return s
+ct = np.vectorize(convertTime)
+
+def main(argv):
+    mainDir = '/home/bjs66/csi/bjs-analysis'
+    run = argv[1]
+    # Add dataset / group to the hdf5 file
+    keys = ['timestamp','bl-csi','bl-mv','pe-pt','pe-roi','pe-iw','arrival','charge','ll-real','ll-flat','rt10','rt50','rt90','muon1','muon2','muon3','pe-total']
+    
+    # What data type is necessary to save the information provided in the csv files. Mainly used to save some storage space 
+    datatypes = {'timestamp': np.dtype(np.float),
+                 'bl-csi': np.dtype(np.int16),
+                 'bl-mv': np.dtype(np.int16),
+                 'pe-pt': np.dtype(np.uint16),
+                 'pe-roi': np.dtype(np.uint16),
+                 'pe-iw': np.dtype(np.uint16),                   
+                 'arrival': np.dtype(np.uint32),
+                 'charge': np.dtype(np.uint64),
+                 'll-real': np.dtype(np.float),
+                 'll-flat': np.dtype(np.float),
+                 'rt10': np.dtype(np.float),
+                 'rt50': np.dtype(np.float),
+                 'rt90': np.dtype(np.float),                     
+                 'muon1': np.dtype(np.int32),
+                 'muon2': np.dtype(np.int32),
+                 'muon3': np.dtype(np.int32),
+                 'pe-total': np.dtype(np.int32),}
+                     
+    days = [x for x in os.listdir('%s/%s'%(mainDir,run)) if '.h5' not in x]
+    
+    print '\nStarting conversion now ...'
+    
+    for d in days:
+        f = h5py.File('%s/%s/%s.h5'%(mainDir,run,d),'w')
+        
+        times = [x.split('-')[1] for x in os.listdir('%s/%s/%s'%(mainDir,run,d)) if 'B-' in x]
+        times = np.asarray(times)
+        print '%i files found for day %s'%(len(times),d)
+        
+        # ==== Process info files ====
+        # Determine hour long sub-times for each day. If there are less than 
+        # half an hour at the end of the day add it to the previous bin
+        i = 0
+        subTimeArray = []
+        while True:
+            if (len(times) - (i+1)*60) >= 30:
+                _subT = times[i*60:(i+1)*60]
+                subTimeArray.append(_subT)
+                if (i+1)*60 > len(times): break
+                i += 1
+            else:
+                _subT = times[i*60:]
+                subTimeArray.append(_subT)
+                break
+        subTimeArray = np.asarray(subTimeArray)
+        
+        # For each sub-time merge all distribution data and concatenate the 
+        # important numbers
+        for sTA in subTimeArray:
+            wfProcessed = []
+            linGates = []
+            overflows = []
+            muonVetos = []
+            bWinAnalyzed = []
+            sWinAnalyzed = []
+            csiBaseline = []
+            muon_hits = np.array([])
+            bP_charge = np.array([])
+            bP_onset = np.array([])
+
+            yspe = np.zeros(300)
+            ppt_bg = np.zeros(52)
+            ppt_s = np.zeros(52)
+            csi_bl = np.zeros(32)
+            charge_dist = np.zeros((350,350))
+            peak_width_distribution = np.zeros(51)
+            peak_amplitude_distribution = np.zeros(100)
+                
+            # Read data from file...
+            for t in sTA:
+                if os.stat('%s/%s/%s/%s-%s'%(mainDir,run,d,'I',t)) > 0 and os.path.getsize('%s/%s/%s/%s-%s'%(mainDir,run,d,'S',t)) > 0: 
+                    with open('%s/%s/%s/%s-%s'%(mainDir,run,d,'I',t),'r') as fIn:
+                        current_charge = 0
+                        read_charge_dist = False
+                        for idx,line in enumerate(fIn):
+                            if idx == 1:
+                                wfProcessed.append(int(line))
+                            elif idx == 3:
+                                linGates.append(int(line))
+                            elif idx == 5:
+                                overflows.append(int(line))
+                            elif idx == 7:
+                                muonVetos.append(int(line))
+                            elif idx == 9:
+                                bWinAnalyzed.append(int(line))
+                            elif idx == 11:
+                                sWinAnalyzed.append(int(line))
+                            elif idx == 33:
+                                yspe = yspe + np.asarray(line.split(),dtype=int)
+                            elif idx == 35:
+                                ppt_bg = ppt_bg + np.asarray(line.split(),dtype=int)
+                            elif idx == 37:
+                                ppt_s = ppt_s + np.asarray(line.split(),dtype=int)
+                            elif idx == 39:
+                                csi_bl = csi_bl + np.asarray(line.split(),dtype=int)
+                                try:
+                                    csiBaseline.append(np.average(np.arange(85,117),weights=(np.asarray(line.split(),dtype=int))))
+                                except:
+                                    print t
+                            elif idx == 41:
+                                peak_width_distribution = peak_width_distribution + np.asarray(line.split(),dtype=int)
+                            elif idx == 43:
+                                peak_amplitude_distribution = peak_amplitude_distribution + np.asarray(line.split(),dtype=int)
+                            elif idx == 45:
+                                bP_onset = np.concatenate((bP_onset,np.asarray(line.split(),dtype=int)))
+                            elif idx == 47:
+                                bP_charge = np.concatenate((bP_charge,np.asarray(line.split(),dtype=int)))
+                            elif idx == 49:
+                                muon_hits = np.concatenate((muon_hits,np.asarray(line.split(),dtype=int)))
+                            elif idx >= 50:
+                                if 'Charge distribution in full waveform' in line:
+                                    read_charge_dist = True
+                                elif read_charge_dist:
+                                    _td = np.asarray(line.split(),dtype=int)
+                                    current_charge =  _td[0]
+                                    non_zero_locations = _td[1::2]
+                                    non_zero_value = _td[2::2]
+                                    charge_dist[current_charge][non_zero_locations] = charge_dist[current_charge][non_zero_locations] + non_zero_value     
+            if len(wfProcessed) > 0:
+                f.create_dataset('/I/%s/waveformsProcessed'%sTA[0],data=wfProcessed)
+                f.create_dataset('/I/%s/linearGates'%sTA[0],data=linGates)
+                f.create_dataset('/I/%s/overflows'%sTA[0],data=overflows)
+                f.create_dataset('/I/%s/bWindowsAnalyzed'%sTA[0],data=bWinAnalyzed)
+                f.create_dataset('/I/%s/sWindowsAnalyzed'%sTA[0],data=sWinAnalyzed)
+                f.create_dataset('/I/%s/speChargeDist'%sTA[0],data=yspe)
+                f.create_dataset('/I/%s/bPeaksPTDist'%sTA[0],data=ppt_bg)
+                f.create_dataset('/I/%s/sPeaksPTDist'%sTA[0],data=ppt_s)
+                f.create_dataset('/I/%s/csiBaselineDist'%sTA[0],data=csi_bl)
+                f.create_dataset('/I/%s/csiBaseline'%sTA[0],data=csiBaseline)
+                f.create_dataset('/I/%s/peakWidthDist'%sTA[0],data=peak_width_distribution)
+                f.create_dataset('/I/%s/peakAmplitudeDist'%sTA[0],data=peak_amplitude_distribution)
+                f.create_dataset('/I/%s/bigPulseOnset'%sTA[0],data=bP_onset)
+                f.create_dataset('/I/%s/bigPulseCharge'%sTA[0],data=bP_charge)
+                f.create_dataset('/I/%s/muonHits'%sTA[0],data=muon_hits)
+                f.create_dataset('/I/%s/chargeDist2d'%sTA[0],data=charge_dist)
+           
+            
+        for w in ['B','S']:
+            create_Dataset = True
+            i0 = 0
+            dset = {}  
+            f.create_group(w)
+            for idx,t in enumerate(times):
+                try:
+                    if os.path.getsize('%s/%s/%s/%s-%s'%(mainDir,run,d,w,t)) > 0:
+                        _tdata = np.loadtxt('%s/%s/%s/%s-%s'%(mainDir,run,d,w,t)).T
+                        if not np.isscalar(_tdata[0]):
+                            _tdata[0] = ct(_tdata[0])
+                            if create_Dataset:
+                                create_Dataset = False
+                                for i,k in enumerate(keys):
+                                    dset[k] = f.create_dataset('/%s/%s'%(w,k),data=_tdata[i],dtype=datatypes[k],maxshape=(None,))      
+                            else:
+                                for i,k in enumerate(keys):
+                                    dset[k].resize((i0 + len(_tdata[i]),))
+                                    dset[k][i0:] = _tdata[i]
+                            i0 += len(_tdata[0])
+                            del _tdata    
+                    else:
+                        continue
+                except OSError:
+                     continue
+        f.close()
+    print 'Done!'
+
+if __name__ == '__main__':
+    main(sys.argv)
--- convert-output-to-hdf5.py
+++ convert-output-to-hdf5.py
@@ -1,20 +1,22 @@
+#!/home/bjs66/anaconda2/bin/python2.7
+
 import os
 import time as tm
 
 # Handles the creation of condor files for a given set of directories
 # -----------------------------------------------------------------------------
-def createCondorFile(dataDir,outDir,run,day,times):
+def createCondorFile(run):
     # Condor submission file name convention: run-day-time.condor
-    with open('/home/bjs66/CondorFiles/%s-%s.condor'%(run,day),'w') as f:
+    with open('/home/bjs66/CondorFiles/Convert-%s.condor'%run,'w') as f:
         
         # Fixed program location'
-        f.write('Executable = /home/bjs66/GitHub/sns-analysis/sns-analysis-v4\n')
+        f.write('Executable = _convertData.py\n') #/home/bjs66/anaconda2/bin/python2.7\n')
         
         # Arguments passed to the exe:
         # Set main run directory, e.g. Run-15-10-02-27-32-23/151002
 	    # Set current time to be analzyed (w/o .zip extension!), e.g. 184502
 	    # Set output directory, eg Output/ Run-15-10-02-27-32-23/151002
-        f.write('Arguments = \"1 %s $(Process) %s 0\"\n'%(dataDir,outDir)) 
+        f.write('Arguments = \"%s\"\n'%run) 
         
         # Standard cluster universe
         f.write('universe   = vanilla\n')
@@ -24,9 +26,9 @@ def createCondorFile(dataDir,outDir,run,day,times):
         f.write('request_memory = 300\n')
      
         # Output, error and log name convention: run-day-time.log/out/err
-        f.write('log = ../../Logs/%s-%s-$(Process).log\n'%(run,day))
-        f.write('Output = ../../Outs/%s-%s-$(Process).out\n'%(run,day))
-        f.write('Error = ../../Errs/%s-%s-$(Process).err\n'%(run,day))
+        f.write('log = ../../Logs/Test.log\n')
+        f.write('Output = ../../Outs/Test.out\n')
+        f.write('Error = ../../Errs/Test.err\n')
         
         # Do not write any emails
         f.write('notification = never\n')
@@ -34,17 +36,11 @@ def createCondorFile(dataDir,outDir,run,day,times):
         f.write('should_transfer_files = NO\n')
         
         # Add single job to queue
-        f.write('Queue %i'%times)
+        f.write('Queue')
 
 # Main function handling all internals
 # -----------------------------------------------------------------------------
 def main():       
-    # Choose main directory, i.e. ~/csi/beam_on_data/Run-15-06-25-xyz/
-    mainRunDir = '/var/phy/project/phil/grayson/COHERENT/CsI/'
-    
-    # Choose output directory, i.e. ~/output/Run-15-06-25-xyz/
-    mainOutDir = '/var/phy/project/phil/grayson/COHERENT/CsI/bjs-analysis/'
-
     # Choose run to analyze
 #    run = 'Run-15-06-25-12-53-44'
 #    run = 'Run-15-06-26-11-23-13'
@@ -63,44 +59,14 @@ def main():
 #    run = 'Run-15-12-26-08-30-40'
 #    run = 'Run-16-01-07-12-16-36'
 #    runDirs = ['Run-15-08-31-00-23-36','Run-15-09-23-21-16-00','Run-15-10-03-09-26-22','Run-15-10-13-13-27-09'] 
-#    runDirs = ['Run-15-06-26-11-23-13','Run-15-07-31-18-30-14'] 
+    runDirs = ['Run-15-06-26-11-23-13','Run-15-07-31-18-30-14'] 
 #    runDirs = ['Run-15-10-21-13-12-27','Run-15-10-29-15-56-36','Run-15-11-09-11-30-13','Run-15-11-20-11-34-48','Run-15-11-24-15-35-32','Run-15-12-14-11-21-45']
-    runDirs = ['Run-15-12-26-08-30-40','Run-16-01-07-12-16-36','Run-16-02-02-16-26-26','Run-16-02-15-13-46-34']
-    subdirs = {}
-    days_in = {}
     for run in runDirs:
-        possibleSubDirs = ['beam_off_data','beam_on_data','sns_data']
-        for psd in possibleSubDirs:
-            possibleRuns = os.listdir(mainRunDir + psd)
-	    print possibleRuns
-            if run in possibleRuns:
-                subdirs[run] = psd
-                days_in[run] = [x for x in os.listdir(mainRunDir + psd + '/' + run) if 'Settings' not in x]
-            
-    print subdirs
-    print days_in
-
-    for run in runDirs:
-#    Iterate through all days in a given run folder, create a condor file and run it.                
-        for day in days_in[run]:
-
-            # Prepare paths for further processing
-            dataRunDir = mainRunDir + '%s/%s/%s'%(subdirs[run],run,day)
-            outDir = mainOutDir + '%s/%s'%(run,day)
-        
-            # Create output directory if it does not exist
-            if not os.path.exists(outDir):
-                os.makedirs(outDir)    
-
-            # Get all times within the day folder chosen and prepare condor submit files
-            tList = [x.split('.')[0] for x in os.listdir(dataRunDir)]
-            createCondorFile(dataRunDir,outDir,run,day,len(tList))
-            #createCondorFile(dataRunDir,outDir,run,day,2)
-            cmd = 'condor_submit /home/bjs66/CondorFiles/%s-%s.condor'%(run,day)
-            #print cmd
-            os.system(cmd)
-            tm.sleep(1)
- 
+        createCondorFile(run)
+        cmd = 'condor_submit /home/bjs66/CondorFiles/Convert-%s.condor'%run
+        os.system(cmd)
+        tm.sleep(1
+)
 if __name__ == '__main__':
     main()
 
