2514640ec4a4e4ce625c68e9e4c55118cefcf6a0
Nablaquabla <nablaquabla@live.com>
2017-01-26 13:09:02 -0500

    Updated runs

--- _reduceDataSet.py
+++ _reduceDataSet.py
@@ -5,41 +5,61 @@ import numpy as np
 import os
 import sys
 
-sbKeys = ['arrival','bl-csi','charge','ll-flat','ll-real','muon1','pe-iw',
-          'pe-pt','pe-roi','pe-total','rt10','rt50','rt90','timestamp']
-iKeys = ['bigPulseCharge','muonHits','linearGates','overflows','speChargeDist',
-         'waveformsProcessed','bPeaksPTDist','sPeaksPTDist','bigPulseOnset']
+sbKeys = ['charge','arrival','pe-pt','pe-iw','speQindex','rt10','rt50','rt90',
+          'beamOn','muon1','timestamp']
+speqKeys = ['GaussBest','PolyaBest']
 
 def main(args):
     run = args[1]
 
-    # Determine day files corresponding to the run
-    h5DataFiles = [x for x in os.listdir('./%s'%run) if '.h5' in x]
-
+    # Get all days in input run dir
+    h5DataFiles = [x for x in os.listdir('/home/bjs66/csi/bjs-analysis/Processed/%s'%run) if '.h5' in x]
+    
     # Read each day file and process the hourly data
     for h5f in h5DataFiles:
         day = h5f.split('.')[0]
-        h5Out = h5py.File('./Processed/%s/%s-ReducedData.h5'%day,'w')
-        f = h5py.File('./%s/%s'%(run,h5f),'r')
+        h5Out = h5py.File('/home/bjs66/csi/bjs-analysis/Processed/ReducedData/%s/%s-ReducedData.h5'%(run,day),'w')
+        h5In = h5py.File('/home/bjs66/csi/bjs-analysis/Processed/%s/%s'%(run,h5f),'r')
+        
+        speqG = h5In['SPEQ/GaussBest'][...][:,0]
+        speqP = h5In['SPEQ/PolyaBest'][...][:,0]
+        speqTimes = h5In['SPEQ/Times'][...]
+
         for wd in ['S','B']:
-            piw = f['/%s/pe-iw'%wd][...]
-            ciw = piw >= 5
-            mu = f['/%s/muon2'%wd][...]
-            cmu = mu == -1
-            cut = ciw * cmu
+            # Convert charge to number if photoelectrons in ROI
+            speQindex = h5In['/%s/speQindex'%wd][...]
+            charge = h5In['/%s/charge'%wd][...]
+            npe = 1.0*charge/speqG[speQindex]
+    
+            # Get number of peaks in the ROI
+            piw = h5In['/%s/pe-iw'%wd][...]
+    
+            # Get potential index for second muon
+            mu = h5In['/%s/muon2'%wd][...]
+
+            # There are less than 40 NPE from charge integration in the ROI
+            cutNPE = (npe < 40)
+
+            # There are less than 4 peaks in the ROI
+            cutPIW = (piw < 4)
+
+            # There are more than one muon in the trace
+            cutMuon = (mu != -1)
+
+            # Full cut:
+            # Cut everything with more than one muon
+            # Cut all events with less than 4 peaks in IW iff there are less than 40 NPE from charge integration in IW
+            cutTotal = np.logical_not(np.asarray(cutMuon + cutNPE * cutPIW,dtype=bool))
 
             # Copy cut signal and background data to new HDF5 file
             for data in sbKeys:
-                h5Out.create_dataset('/%s/%s'%(wd,data),data=f['/%s/%s'%(wd,data)][...][cut])
-
-
-            for time in np.sort(h5Out['/I'].keys()):
-                for key in iKeys:
-                    h5Out.create_dataset('/I/%s/%s'%(time,key),data=f['/I/%s/%s'%(time,key)][...])
-
+                h5Out.create_dataset('/%s/%s'%(wd,data),data=h5In['/%s/%s'%(wd,data)][...][cutTotal])
+        h5Out.create_dataset('/speq/gauss',data=speqG)
+        h5Out.create_dataset('/speq/polya',data=speqP)
+        h5Out.create_dataset('/speq/times',data=speqTimes)
         # Properly close hdf5 files
         h5Out.close()
-        f.close()
+        h5In.close()
 
 # ============================================================================
 #                                Run program
--- reduceDataSet.py
+++ reduceDataSet.py
@@ -7,7 +7,7 @@ import time as tm
 # -----------------------------------------------------------------------------
 def createCondorFile(run):
     # Condor submission file name convention: run-day-time.condor
-    with open('/home/bjs66/CondorFiles/ReduceDS-%s.condor'%run,'w') as f:
+    with open('/home/bjs66/CondorFiles/ReduceDataSet-%s.condor'%run,'w') as f:
         
         # Fixed program location'
         f.write('Executable = _reduceDataSet.py\n') #/home/bjs66/anaconda2/bin/python2.7\n')
@@ -26,9 +26,9 @@ def createCondorFile(run):
         f.write('request_memory = 300\n')
      
         # Output, error and log name convention: run-day-time.log/out/err
-        f.write('log = /home/bjs66/Logs/ReduceDS-%s.log\n'%run)
-        f.write('Output = /home/bjs66/Outs/ReduceDS-%s.out\n'%run)
-        f.write('Error = /home/bjs66/Errs/ReduceDS-%s.err\n'%run)
+        f.write('log = /home/bjs66/Logs/ReduceDataSet-%s.log\n'%run)
+        f.write('Output = /home/bjs66/Outs/ReduceDataSet-%s.out\n'%run)
+        f.write('Error = /home/bjs66/Errs/ReduceDataSet-%s.err\n'%run)
         
         # Do not write any emails
         f.write('notification = never\n')
@@ -42,17 +42,30 @@ def createCondorFile(run):
 # -----------------------------------------------------------------------------
 def main():       
     # Choose run to analyze
-#    runDirs = ['Run-15-06-25-12-53-44','Run-15-06-26-11-23-13','Run-15-07-31-18-30-14','Run-15-08-18-14-51-18',
-#               'Run-15-08-31-00-23-36','Run-15-09-21-20-58-01','Run-15-09-23-21-16-00','Run-15-10-03-09-26-22',
-#               'Run-15-10-13-13-27-09','Run-15-10-21-13-12-27','Run-15-10-29-15-56-36','Run-15-11-09-11-30-13',
-#               'Run-15-11-20-11-34-48','Run-15-11-24-15-35-32','Run-15-12-14-11-21-45'] 
-#    runDirs = ['Run-15-06-25-12-53-44','Run-15-06-26-11-23-13','Run-15-07-31-18-30-14',
-#               'Run-15-11-20-11-34-48','Run-15-11-24-15-35-32','Run-15-12-14-11-21-45'] 
-    runDirs = ['Run-15-06-25-12-53-44','Run-15-09-21-20-58-01']
-               
+#    runDirs = ['Run-15-06-25-12-53-44']
+#    runDirs = ['Run-15-06-26-11-23-13','Run-15-07-31-18-30-14']
+#    runDirs = ['Run-15-08-18-14-51-18','Run-15-08-31-00-23-36','Run-15-09-21-20-58-01']
+#    runDirs = ['Run-15-09-23-21-16-00','Run-15-10-03-09-26-22','Run-15-10-13-13-27-09']
+#    runDirs = ['Run-15-10-21-13-12-27','Run-15-10-29-15-56-36','Run-15-11-09-11-30-13']
+#    runDirs = ['Run-15-11-20-11-34-48','Run-15-11-24-15-35-32','Run-15-12-14-11-21-45']
+#    runDirs = ['Run-15-12-26-08-30-40','Run-16-01-07-12-16-36','Run-16-02-02-16-26-26']
+#    runDirs = ['Run-16-02-15-13-46-34','Run-16-02-29-11-54-20','Run-16-03-09-13-00-14']
+#    runDirs = ['Run-16-03-22-18-09-33','Run-16-03-30-12-44-57','Run-16-04-12-11-54-27']
+#    runDirs = ['Run-16-04-20-11-22-48','Run-16-05-05-14-08-52','Run-16-05-12-14-07-59']
+#    runDirs = ['Run-16-05-17-14-40-34','Run-16-06-02-12-35-56','Run-16-06-17-12-09-12']
+#    runDirs = ['Run-16-06-27-17-50-08','Run-16-07-06-18-25-19','Run-16-07-12-11-44-55']
+    runDirs = ['Run-16-07-18-11-50-24','Run-16-07-21-11-59-39','Run-16-07-28-12-49-17']
+#    runDirs = ['Run-16-07-06-18-25-19']
+            
     for run in runDirs:
+
+        # Create output run dir
+        cmd = 'mkdir -p /home/bjs66/csi/bjs-analysis/Processed/ReducedData/%s'%run
+        os.system(cmd)
+
+
         createCondorFile(run)
-        cmd = 'condor_submit /home/bjs66/CondorFiles/ReduceDS-%s.condor'%run
+        cmd = 'condor_submit /home/bjs66/CondorFiles/ReduceDataSet-%s.condor'%run
         os.system(cmd)
         tm.sleep(1)
         
