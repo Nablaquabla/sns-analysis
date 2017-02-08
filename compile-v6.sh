5bffccc8a59cb93d561d376191d7e6f9b4155427
Nablaquabla <nablaquabla@live.com>
2017-02-08 14:44:15 -0500

    Added compile script

---
+++ compile-v6.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+g++ sns-analysis-v6.cpp -lzip -L /home/bjs66/usr/bin/libzip/lib/ -I /home/bjs66/usr/bin/libzip/include/ -I /home/bjs66/usr/bin/libzip/lib/libzip/include/ -o sns-analysis-v6
